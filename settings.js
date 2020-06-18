36da4956968589fbbc1c533751ad941208fc8236
Gabriel Brun <lydstyl@gmail.com>
2020-06-18 22:10:53 +0200

    api

--- settings.js
+++ settings.js
@@ -1,18 +1,18 @@
 const settings = {
-  inputImagesFolder: '/home/gab/06/605',
+  inputImagesFolder: "/home/gab/imagesFolder",
   width: 1200, // 1080, 1366, 1440, 1920, 2160, 2560, 3840
   greyscale: false,
-  outputExt: 'jpg', // 'auto, webp, jpg, png'
+  outputExt: "jpg", // 'auto, webp, jpg, png'
 };
 
 exports.settings = settings;
 
-exports.outputExts = ['jpg', 'webp'];
+exports.outputExts = ["jpg", "webp"];
 
 exports.settingsKeys = Object.keys(settings).reverse();
 
 exports.setOutputImagesFolder = () => {
   settings.outputImagesFolder = `${settings.inputImagesFolder}/${
     settings.outputExt
-  }_${settings.width}${settings.greyscale ? '_greyscale' : ''}`;
+  }_${settings.width}${settings.greyscale ? "_greyscale" : ""}`;
 };
